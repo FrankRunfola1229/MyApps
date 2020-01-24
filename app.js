881217ae2ee5e08623b842fa40e2003abd1614b2
Frank Runfola <Frunfola1229@gmail.com>
2020-01-24 17:56:40 -0500

    add new images folder

--- app.js
+++ app.js
@@ -6,63 +6,62 @@ const app = express()
 //==================================================================================================
 app.use(express.static("public")) //https://expressjs.com/en/starter/static-files.html
 app.use(express.static("data"))
-app.use(express.static("Images"))
 
 // view engine setup
 app.set("view engine", "ejs")
 
 // root route
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
    res.render("index")
 })
 
 // nfl route
-app.get("/nfl", function(req, res) {
+app.get("/nfl", function (req, res) {
    res.render("nfl")
 })
 
 // root route
-app.get("/movies", function(req, res) {
+app.get("/movies", function (req, res) {
    res.render("movies")
 })
 
 // tools route
-app.get("/tools", function(req, res) {
+app.get("/tools", function (req, res) {
    res.render("tools")
 })
 
 //familyTree route
-app.get("/familyTree", function(req, res) {
+app.get("/familyTree", function (req, res) {
    res.render("familyTree")
 })
 
 // music route
-app.get("/music", function(req, res) {
+app.get("/music", function (req, res) {
    res.render("music")
 })
 
 // passing route
-app.get("/passing", function(req, res) {
+app.get("/passing", function (req, res) {
    res.render("passing")
 })
 
 // receiving route
-app.get("/receiving", function(req, res) {
+app.get("/receiving", function (req, res) {
    res.render("receiving")
 })
 
 // defense route
-app.get("/defense", function(req, res) {
+app.get("/defense", function (req, res) {
    res.render("defense")
 })
 
-app.get("/flex", function(req, res) {
+app.get("/flex", function (req, res) {
    res.render("flex")
 })
 
 var port = process.env.PORT || 3000
 
-app.listen(port, function() {
+app.listen(port, function () {
    console.log("The Server Has Started!..")
    console.log("Port= " + port)
 })
