38357fbce844d1928eb6672f5411506b0a3591ed
Runfola,Frank <runfola.frank@healthnow.org>
2020-01-27 13:25:54 -0500

    fixed nav bar

--- app.js
+++ app.js
@@ -11,57 +11,72 @@ app.use(express.static("data"))
 app.set("view engine", "ejs")
 
 // root route
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
    res.render("index")
 })
 
 // nfl route
-app.get("/sports", function (req, res) {
+app.get("/sports", function(req, res) {
    res.render("sports")
 })
 
 // root route
-app.get("/movies", function (req, res) {
+app.get("/movies", function(req, res) {
    res.render("movies")
 })
 
 // tools route
-app.get("/tools", function (req, res) {
+app.get("/tools", function(req, res) {
    res.render("tools")
 })
 
 //familyTree route
-app.get("/familyTree", function (req, res) {
+app.get("/familyTree", function(req, res) {
    res.render("familyTree")
 })
 
 // music route
-app.get("/music", function (req, res) {
+app.get("/music", function(req, res) {
    res.render("music")
 })
 
+// music route
+app.get("/computerScience", function(req, res) {
+   res.render("computerScience")
+})
+
+// music route
+app.get("/geography", function(req, res) {
+   res.render("geography")
+})
+
 // passing route
-app.get("/passing", function (req, res) {
+app.get("/cars", function(req, res) {
+   res.render("cars")
+})
+
+// passing route
+app.get("/passing", function(req, res) {
    res.render("passing")
 })
 
 // receiving route
-app.get("/receiving", function (req, res) {
+app.get("/receiving", function(req, res) {
    res.render("receiving")
 })
 
 // defense route
-app.get("/defense", function (req, res) {
+app.get("/defense", function(req, res) {
    res.render("defense")
 })
 
-app.get("/flex", function (req, res) {
+app.get("/flex", function(req, res) {
    res.render("flex")
 })
 
 var port = process.env.PORT || 3000
 
-app.listen(port, function () {
+app.listen(port, function() {
    console.log("The Server Has Started!..")
    console.log("Port= " + port)
 })
