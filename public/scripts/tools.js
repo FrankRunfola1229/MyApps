679fdd7c5cfb462427c0eb2b7ae14018581d4c35
Runfola,Frank <runfola.frank@healthnow.org>
2020-02-04 17:43:06 -0500

    Add percent logic

--- public/scripts/tools.js
+++ public/scripts/tools.js
@@ -1,24 +1,19 @@
-// Create Table Header elem
-const createTh = (data, tr) => {
-   const th = document.createElement("th")
-   th.innerText = data
-   th.setAttribute("scope", "row")
-   tr.appendChild(th)
-}
+const body = document.createElement("body")
+const h1 = document.createElement("h1")
 
+h1.innerText = data
+body.appendChild(h1)
 
-function
-perc1() {
-   a = document.form1.a.value / 100;
-   b = a * document.form1.b.value;
+const percentage1 = (data, tr) => {
+   a = document.form1.a.value / 100
+   b = a * document.form1.b.value
    document.form1.total1.value = b
 }
 
-function
-perc2() {
-   a = document.form1.c.value;
-   b = document.form1.d.value;
-   c = a / b;
-   d = c * 100;
+const percentage2 = (data, tr) => {
+   a = document.form1.c.value
+   b = document.form1.d.value
+   c = a / b
+   d = c * 100
    document.form1.total2.value = d
 }
