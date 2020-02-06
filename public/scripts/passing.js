c914a7d9aca2c9d05b08b21e73a3b12ac853cf05
Frank Runfola <frunfola1229@gmail.com>
2020-02-06 17:36:28 -0500

    Update passing.js

--- public/scripts/passing.js
+++ public/scripts/passing.js
@@ -3,7 +3,7 @@ const tbody = document.querySelector("tbody")
 let count = 0
 
 const readJsonFile = () => {
-   fetch("/PassingData.json") //
+   fetch("/../PassingData.json") //
       .then(response => response.json())
       .then(data => createTable(data))
       .catch(err => {})
