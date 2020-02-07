8db41ff17a141f43196da584b23c62f92bc9e5f7
Frank Runfola <frunfola1229@gmail.com>
2020-02-06 19:08:31 -0500

    Update movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 // root route
-router.get("/", function(req, res) {
+router.get("/", (req, res) => {
    res.render("movies")
 })
 module.exports = router
