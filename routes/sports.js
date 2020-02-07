371b297fdec71811dcb69b37d9579f543b599c8a
Frank Runfola <frunfola1229@gmail.com>
2020-02-06 19:45:15 -0500

    Update sports.js

--- routes/sports.js
+++ routes/sports.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 // nfl route
-router.get("/", (req, res) => {
+router.get("/", function(req, res){
    res.render("sports")
 })
 
