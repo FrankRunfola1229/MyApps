eb831041cba068021e98969eb1262717977088ac
Frank Runfola <frunfola1229@gmail.com>
2020-02-06 19:07:54 -0500

    Update familyTree.js

--- routes/familyTree.js
+++ routes/familyTree.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 //familyTree route
-router.get("/", function(req, res) {
+router.get("/", (req, res) => {
    res.render("familyTree")
 })
 module.exports = router
