37cee73db760ef6d26006568010732f7305c5d42
Frank Runfola <frunfola1229@gmail.com>
2020-02-06 19:44:39 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 // root route
-router.get("/", (req, res) => {
+router.get("/", function(req, res){
    res.render("index")
 })
 module.exports = router
