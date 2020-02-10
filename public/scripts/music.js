e5930c3bcc2c4d023478f9124765042913c2523c
Frank Runfola <frunfola1229@gmail.com>
2020-02-10 17:20:15 -0500

    Update music.js

--- public/scripts/music.js
+++ public/scripts/music.js
@@ -1,4 +1,6 @@
+// ------------------------------------------------------------
 // set the dimensions and margins of the graph
+// ------------------------------------------------------------
 const margin = { top: 20, right: 20, bottom: 30, left: 40 }
 const width = 960 - margin.left - margin.right
 const height = 500 - margin.top - margin.bottom
